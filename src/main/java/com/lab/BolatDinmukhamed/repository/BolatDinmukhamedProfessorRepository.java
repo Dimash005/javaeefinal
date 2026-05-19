9b4efa58f1b33c0d851b64f49fc51ea37b998540
Dimash005 <bolatdimashhh@gmail.com>
2026-05-19 21:22:20 +0500

    add ProfessorRepository

---
+++ src/main/java/com/lab/BolatDinmukhamed/repository/BolatDinmukhamedProfessorRepository.java
@@ -0,0 +1,9 @@
+package com.lab.BolatDinmukhamed.repository;
+
+import com.lab.BolatDinmukhamed.entity.BolatDinmukhamedProfessor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BolatDinmukhamedProfessorRepository extends JpaRepository<BolatDinmukhamedProfessor, Long> {
+}
